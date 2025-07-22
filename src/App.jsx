1a189166ca7cd4cd7b1b614959cf711a7ff0171d
milodev <milodev09@gmail.com>
2025-07-21 23:47:13 -0500

    mod: init with login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,11 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-
-// Context
-//import { ProductosProvider } from './context/ProductosProvider'
-
-// Layout
-//import LayoutAuth from './Layout/LayoutAuth'
-
-// pages (productos)
-//import ListaProductos from './pages/productos/ListaProductos'
-//import FormularioProductos from './pages/productos/FormularioProductos'
-//import DetalleProducto from './pages/productos/DetalleProducto'
+// App.jsx
+import { BrowserRouter as Router } from 'react-router-dom'
+import Login from './pages/Login'
 
 function App() {
   return (
     <Router>
-      <ProductosProvider>
-        <Routes>
-          <Route path='/' element={<LayoutAuth />}>
-            <Route index element={<ListaProductos />} />
-          </Route>
-          <Route path='/products' element={<LayoutAuth />}>
-            <Route index element={<ListaProductos />} />
-            <Route path='agregar-producto' element={<FormularioProductos />} />
-            <Route path='detalle-producto/:id' element={<DetalleProducto />} />
-          </Route>
-        </Routes>
-      </ProductosProvider>
+      <Login />
     </Router>
   )
 }
