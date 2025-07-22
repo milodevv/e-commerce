f66abc71405deaad20f64a18e4884ac59dce7184
milodev <milodev09@gmail.com>
2025-07-22 06:14:35 -0500

    add: footer

---
+++ src/pages/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <footer className="bg-black text-white text-center py-6 mt-10">
+            <p>&copy; {new Date().getFullYear()} Vibra Store. Todos los derechos reservados.</p>
+        </footer>
+    );
+};
+
+export default Footer;
